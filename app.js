3b781d91d1433249c272041e926c01a2842fb5e2
amin_ruhul <ruhulinfo50@gmail.com>
2021-05-16 18:01:51 +0600

    connected to DB

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const postRouter = require("./routes/post");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv/config");
 
 const PORT = 5000;
 
 app.use("/post", postRouter);
+
+mongoose.connect(
+  process.env.MONGO_DB_URLS,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log("connected to DB");
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("Home Page");
